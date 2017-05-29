78209df7e4f44063306ac71a256649da6ef9344e
elizaveta28 <lizaveta40@mail.ru>
2017-05-29 23:07:09 +0300

    Define model

--- server.js
+++ server.js
@@ -5,13 +5,17 @@ var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
-mongoose.connect('mongodb://lizaveta40:28l06i19z97a@ds133981.mlab.com:33981/web-scraper');
+mongoose.connect('mongodb://lizaveta40:28l06i19z97a@ds155811.mlab.com:55811/todos');
 app.use(express.static(__dirname + '/public'));
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({'extended':'true'}));
 app.use(bodyParser.json());
 app.use(bodyParser.json({type : 'application/vnd.api+json'}));
 app.use(methodOverride());
+
+var Todo = mongoose.model('Todo', {
+	text : String
+});
  
 app.listen(8080);
 console.log("App listening on port 8080...");
